390544262a4f14a8ed816ffec12013c7a207a6f6
SunithaBabu <sunithab@wilcosource.com>
2021-04-23 11:14:32 +0200

    Included StyleCop analysers

--- PromotionEngine/PromotionEngine/Engine.cs
+++ PromotionEngine/PromotionEngine/Engine.cs
@@ -1,7 +1,15 @@
-﻿namespace PromotionEngine
+﻿//-----------------------------------------------------------------------
+// <copyright file="Engine.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace PromotionEngine
 {
     using System;
 
+    /// <summary>
+    /// Engine Class.
+    /// </summary>
     public class Engine
     {
     }
