d56e9a345a3004b26be692affa18e9d7bcf5c154
SunithaBabu <sunithab@wilcosource.com>
2021-04-23 11:42:29 +0200

    Included Xunit Test project

---
+++ PromotionEngine/PromotionEngineTest/EngineTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PromotionEngineTest
+{
+    public class EngineTest
+    {
+        [Fact]
+        public void CalcuateTotalOrderPrice()
+        {
+
+        }
+    }
+}
